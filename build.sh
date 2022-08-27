82c33630a521fb41d3205d9582a328f27f948792
Kashish Garg <kashishgarg9@gmail.com>
2022-08-27 18:42:45 -0400

    Working Test function bash

--- build.sh
+++ build.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+RED=$'\e[1;31m'
+GREEN=$'\e[1;32m'
+NC=$'\e[1;0m'
+CYAN=$'\e[1;36m'
+PURPLE=$'\e[1;35m'
+
 # Testing:
 function test() {
   echo "Running Test on Binary: $1";
   ./$1;
 
-  if  [$? -eq 0] 
+  if  [ $? -eq 0 ] 
   then
-    echo "$1 Suceeded!"
+    echo "Testing on $1 $GREEN Suceeded!$NC"
   else
-    echo "$1 Failed!"
+    echo "Testing on $1 $RED Failed!$NC"
   fi
 
 }
@@ -19,4 +25,4 @@ g++ test/JointConfigTest.cpp src/Joint.cpp -I include/ -o joint_config
 test joint_config
 
 
-echo "Done"
+echo "$GREEN Done"
